b54f0c856966376806de16ccbd24cc9679c564a2
Huy Nguyen <huynguyen28497@gmail.com>
2022-03-10 09:19:02 +0700

    feat: upgrade to react router v6

--- src/navigation/RouterConfig.tsx
+++ src/navigation/RouterConfig.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import AboutPage from 'pages/about/AboutPage';
-import HomePage from 'pages/home/HomePage';
-import { Switch, Route } from 'react-router-dom';
-import { ABOUT_PATH, ROOT } from './CONSTANT';
-import NotFound from 'pages/error/NotFound';
+import React from "react";
+import AboutPage from "pages/about/AboutPage";
+import HomePage from "pages/home/HomePage";
+import { Routes, Route } from "react-router-dom";
+import { ABOUT_PATH, ROOT } from "./CONSTANT";
+import NotFound from "pages/error/NotFound";
 
 interface Props {}
 
 const RouterConfig = (props: Props) => {
   return (
     <div>
-      <Switch>
+      <Routes>
         {/* List all public routes here */}
-        <Route exact path={ROOT} component={HomePage} />
-        <Route exact path={ABOUT_PATH} component={AboutPage} />
+        <Route path={ROOT} element={<HomePage />} />
+        <Route path={ABOUT_PATH} element={<AboutPage />} />
 
         {/* List all private/auth routes here */}
 
         {/* List a generic 404-Not Found route here */}
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };

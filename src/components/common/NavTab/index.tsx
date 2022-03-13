ef5429be1b545def862220d7ba941f8b0586f1f4
Huy Nguyen <huynguyen28497@gmail.com>
2022-03-13 13:35:11 +0700

    fix: navbar bottom

--- src/components/common/NavTab/index.tsx
+++ src/components/common/NavTab/index.tsx
@@ -1,8 +1,4 @@
-import {
-  Box,
-  Drawer,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Drawer, Typography } from "@mui/material";
 import { house_icon, logo } from "assets";
 import React, { useEffect } from "react";
 
@@ -25,19 +21,47 @@ const NavTab = ({ open = true, drawerWidth = "15vw" }) => {
       <Box paddingTop={"28px"} paddingBottom={"28px"} paddingLeft={"24px"}>
         <img style={{ width: 42, height: 33 }} src={logo} />
       </Box>
-      <Typography paddingLeft={'14px'} color={"#9CA3AF"}>
+      <Typography paddingLeft={"14px"} color={"#9CA3AF"}>
         General
       </Typography>
-      <Box marginX={"16px"} marginY={"4px"}>
-        <Box
-          display={'flex'}
-          alignItems="center"
-          paddingX={'12px'}
-          borderRadius={"8px"}
-          style={{ background: "rgba(255, 255, 255, 0.08)" }}
-        >
-          <img style={{ width: 16, height: 16, marginRight: 8 }} src={house_icon} />
-          <p style={{ color: '#10B981', fontWeight: 600 }} >Main</p>
+      <Box
+        marginX={"16px"}
+        marginY={"4px"}
+        height="100%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <Box>
+          <Box
+            display={"flex"}
+            alignItems="center"
+            paddingX={"12px"}
+            borderRadius={"8px"}
+            style={{ background: "rgba(255, 255, 255, 0.08)" }}
+          >
+            <img
+              style={{ width: 16, height: 16, marginRight: 8 }}
+              src={house_icon}
+            />
+            <p style={{ color: "#10B981", fontWeight: 600 }}>Main</p>
+          </Box>
+        </Box>
+        <Box display="flex" flexDirection="column" paddingBottom="24px">
+          <Box paddingBottom="16px">
+            <Typography fontSize={"14px"} color={"#fff"}>
+              Need help?
+            </Typography>
+            <Typography fontSize={"14px"} color={"#9CA3AF"}>
+              Check our docs
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#10B981", textTransform: "none" }}
+          >
+            Documentation
+          </Button>
         </Box>
       </Box>
     </Drawer>
